c5b222bb812f59f175154922fdb0231c934cafc5
Luke Granger-Brown <git@lukegb.com>
2014-09-13 21:13:00 +0000

    switches to using a chain SSL certificate context

--- ldapwat.py
+++ ldapwat.py
@@ -1,6 +1,7 @@
 from zope.interface import implements
 
 from twisted.internet.protocol import ServerFactory
+from twisted.internet.ssl import DefaultOpenSSLContextFactory, SSL
 from twisted.python.components import registerAdapter
 from twisted.python import failure
 from twisted.python.modules import getModule
@@ -296,10 +297,30 @@ registerAdapter(lambda x: x.root,
             LDAPServerFactory,
             interfaces.IConnectedLDAPEntry)
 
+class ChainedOpenSSLContextFactory(DefaultOpenSSLContextFactory):
+    def __init__(self, privateKeyFileName, certificateChainFileName,
+                 sslmethod=SSL.TLSv1_METHOD):
+        """
+        @param privateKeyFileName: Name of a file containing a private key
+        @param certificateChainFileName: Name of a file containing a certificate chain
+        @param sslmethod: The SSL method to use
+        """
+        self.privateKeyFileName = privateKeyFileName
+        self.certificateChainFileName = certificateChainFileName
+        self.sslmethod = sslmethod
+        self.cacheContext()
+    
+    def cacheContext(self):
+        ctx = SSL.Context(self.sslmethod)
+        ctx.use_certificate_chain_file(self.certificateChainFileName)
+        ctx.use_privatekey_file(self.privateKeyFileName)
+        self._context = ctx
+
 factory = LDAPServerFactory(tree.db)
-certData = getModule(__name__).filePath.sibling('server.pem').getContent()
-certificate = ssl.PrivateCertificate.loadPEM(certData)
+#certData = getModule(__name__).filePath.sibling('server.pem').getContent()
+#certificate = ssl.PrivateCertificate.loadPEM(certData)
 application = service.Application("ldaptor-server")
 myService = service.IServiceCollection(application)
-reactor.listenSSL(3389, factory, certificate.options())
+#reactor.listenSSL(3389, factory, certificate.options())
+reactor.listenSSL(3389, factory, ChainedOpenSSLContextFactory('server.key', 'server.crt'))
 reactor.run()
